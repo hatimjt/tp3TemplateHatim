d8e72df875657b04eeb9b05d14418f22a5589120
hatimjt <hatimjait1@gmail.com>
2023-11-05 22:36:08 +0100

    config de base(tp2) et Création_modification du template

---
+++ src/main/java/org/emsi/tp3templatehatim/configjsf/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.emsi.tp3templatehatim.configjsf;
+// classe de configuration pour jsf
+
+/**
+ *
+ * @author hatim
+ */
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
